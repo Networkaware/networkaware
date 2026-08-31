660f968cf9a5a00c94debe46d63922fd82289277
Networkaware <ggvosas@gmail.com>
2026-08-31 18:59:00 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const welcomeButton = document.querySelector("#welcome-button");
-const message = document.querySelector("#message");
-const year = document.querySelector("#year");
-
-year.textContent = new Date().getFullYear();
-
-welcomeButton.addEventListener("click", () => {
-  message.textContent = "¡Bienvenido a Network Aware! Estamos construyendo algo grande.";
-});
